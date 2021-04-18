3f7277e895bb941f94c43390eab1c034bae1a07c
WeiLu1 <w.j.lu96@gmail.com>
2021-04-19 00:13:58 +0100

    testing dungeon url

--- Crawler/tests/conftest.py
+++ Crawler/tests/conftest.py
@@ -1,7 +1,1 @@
 import pytest
-import requests
-
-
-@pytest.fixture(scope="session")
-def main_site():
-    return 'https://www.zeldadungeon.net'
--- Crawler/tests/test_all.py
+++ Crawler/tests/test_all.py
@@ -1,13 +1,20 @@
 import requests
 
 
-def test_main_page(main_site):
-    assert requests.get(main_site) is not None
+MAIN_SITE = 'https://www.zeldadungeon.net'
 
 
-def test_character_page(main_site):
-    assert requests.get(main_site + '/wiki/Link') is not None
+def test_main_page():
+    assert requests.get(MAIN_SITE) is not None
 
 
-def test_bosses_page(main_site):
-    assert requests.get(main_site + '/wiki/A_Link_to_the_Past_Bosses') is not None
+def test_character_page():
+    assert requests.get(MAIN_SITE + '/wiki/Link') is not None
+
+
+def test_bosses_page():
+    assert requests.get(MAIN_SITE + '/wiki/A_Link_to_the_Past_Bosses') is not None
+
+
+def test_dungeons_page():
+    assert requests.get(MAIN_SITE + '/wiki/A_Link_to_the_Past_Dungeons') is not None
